68bac64b4e367dfcc9a9d40d4f389d81fc326f9e
Bryansss1 <gregoria1540@gmail.com>
2022-08-16 17:29:35 -0430

    commit 1

---
+++ listadoburger1.js
@@ -0,0 +1,9 @@
+function toggle(){
+      const nav=document.getElementById("menu")
+      menu.classList.toggle("hidden")
+     
+}
+function puto(){
+let alertaXd="eres gay mano"
+alert(alertaXd)
+}
